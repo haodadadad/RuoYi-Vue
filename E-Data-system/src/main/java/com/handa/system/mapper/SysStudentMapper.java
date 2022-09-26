45ea2ff8d869d42074018d3ec43167a8fbfbbaad
haodadadad <gzh1343572674@gmail.com>
2022-09-26 14:15:51 +0800

    修改项目名为E-Data

---
+++ E-Data-system/src/main/java/com/handa/system/mapper/SysStudentMapper.java
@@ -0,0 +1,14 @@
+package com.handa.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.handa.system.domain.SysStudent;
+
+/**
+ * 学生信息Mapper接口
+ *
+ * @author ruoyi
+ */
+public interface SysStudentMapper extends BaseMapper<SysStudent>
+{
+
+}
